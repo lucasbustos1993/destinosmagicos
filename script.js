7a7cf4d26760e9d485e4efbbbee9c2685772bcd6
lucasbustos1993 <luksbustos@gmail.com>
2023-09-17 20:14:18 -0300

    6
    Se agregaron las funciones de los botones de filtros.

--- script.js
+++ script.js
@@ -34,17 +34,71 @@ document.addEventListener("DOMContentLoaded", function() {
                     const backContent = card.querySelector('.back');
                     backContent.innerHTML = `
                         <h2>Paquete Turístico a ${provincia.destino}</h2>
+                        <p>${provincia.texto}</p>
                         <p>Duración: ${provincia.duracion}</p>
-                        <p>Incluye:</p>
-                        <ul>
+                        
                             ${provincia.incluye.map(item => `<li>${item}</li>`).join('')}
-                        </ul>
-                        <button class="contratar-button">Contratar Servicio</button>
+                        
+                            <button class="contratar-button" onclick="window.location.href='suscripcion.html'">Comprar</button>
                     `;
                 }
             });
         });
 });
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener las tarjetas
+    const cards = document.querySelectorAll('.card');
+
+    // Leer el JSON de provincias
+    fetch('provincias.json')
+        .then(response => response.json())
+        .then(data => {
+            // Función de filtro
+            function filtrarTarjetas(destinosMostrar) {
+                cards.forEach(card => {
+                    const nombreProvincia = card.id;
+                    const mostrar = destinosMostrar.includes(nombreProvincia);
+                    card.style.display = mostrar ? 'block' : 'none';
+                });
+            }
+
+            // Asignar eventos a los botones de filtros rápidos
+            const inviernoButton = document.getElementById('inviernoButton');
+            const veranoButton = document.getElementById('veranoButton');
+            const visitadaButton = document.getElementById('visitadaButton');
+
+            inviernoButton.addEventListener('click', function() {
+                const destinosMostrar = data.filter(d => d.invierno).map(d => d.nombre);
+                filtrarTarjetas(destinosMostrar);
+            });
+
+            veranoButton.addEventListener('click', function() {
+                const destinosMostrar = data.filter(d => d.verano).map(d => d.nombre);
+                filtrarTarjetas(destinosMostrar);
+            });
+
+            visitadaButton.addEventListener('click', function(){
+                const destinosMostrar = data.filter(d => d.visitada).map(d => d.nombre);
+                filtrarTarjetas(destinosMostrar);
+            })
+
+            // Restablecer todas las tarjetas cuando se hace clic en "Mostrar Todos"
+            document.getElementById('mostrarTodos').addEventListener('click', function() {
+                cards.forEach(card => {
+                    card.style.display = 'block';
+                });
+            });
+
+            
+        });
+});
+
+
 
 
 
