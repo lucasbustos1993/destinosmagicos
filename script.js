2091d034cbb81dba0c656ced74f9901f5e0572c3
lucasbustos1993 <luksbustos@gmail.com>
2023-10-27 20:40:53 -0300

    script update
    Se agrego una API para envio de formulario

--- script.js
+++ script.js
@@ -92,9 +92,62 @@ function buscar(){
     
 }
 
+/*const $form = document.querySelector('#form');
+$form.addEventListener('submit', handleSubmit)
+async function handleSubmit (event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Acept': 'application/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        alert('Gracias por contactarnos, te escribiremos pronto')
+    }
+}
+*/
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Gracias por contactarnos, te escribiremos pronto";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! Parece que hay un problema"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! Parece que hay un problema"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
 function redireccionar() {
     window.location.href = 'index.html';
+
 }
+
+
 
 
 
