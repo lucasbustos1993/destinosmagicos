3fa184b48ab0f7e3aaa91aa50112ec2265a3fa91
lucasbustos1993 <luksbustos@gmail.com>
2023-10-27 21:32:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -94,24 +94,6 @@ function buscar(){
     
 }
 
-/*const $form = document.querySelector('#form');
-$form.addEventListener('submit', handleSubmit)
-async function handleSubmit (event) {
-    event.preventDefault()
-    const form = new FormData(this)
-    const response = await fetch(this.action, {
-        method: this.method,
-        body: form,
-        headers: {
-            'Acept': 'application/json'
-        }
-    })
-    if (response.ok){
-        this.reset()
-        alert('Gracias por contactarnos, te escribiremos pronto')
-    }
-}
-*/
 
 var form = document.getElementById("my-form");
     
@@ -143,6 +125,8 @@ var form = document.getElementById("my-form");
       });
     }
     form.addEventListener("submit", handleSubmit)
+
+    
 
 function redireccionar() {
     window.location.href = 'index.html';
