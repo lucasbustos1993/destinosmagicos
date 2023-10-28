85e39d25a51f8b138cbc38b6d24b4aad47ba1ecd
lucasbustos1993 <luksbustos@gmail.com>
2023-10-27 21:56:19 -0300

    Update script.js
    se agrego una variable en localstorage para evitar el spam al enviar el formulario

--- script.js
+++ script.js
@@ -96,35 +96,54 @@ function buscar(){
 
 
 var form = document.getElementById("my-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.innerHTML = "Gracias por contactarnos, te escribiremos pronto";
-          form.reset()
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.innerHTML = "Oops! Parece que hay un problema"
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! Parece que hay un problema"
-      });
+var submitButton = document.getElementById("submit-button");
+
+function hasSubmittedForm() {
+  return localStorage.getItem('hasSubmittedForm') === 'true';
+}
+
+function setSubmittedFlag() {
+  localStorage.setItem('hasSubmittedForm', 'true');
+}
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+
+  if (hasSubmittedForm()) {
+    status.innerHTML = "Ya has enviado el formulario.";
+    return;
+  }
+
+  var data = new FormData(event.target);
+
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
     }
-    form.addEventListener("submit", handleSubmit)
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "Gracias por contactarnos, te escribiremos pronto";
+      form.reset();
+      setSubmittedFlag(); // Establece la bandera de envío
+      submitButton.disabled = true; // Deshabilita el botón
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "Oops! Parece que hay un problema"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "Oops! Parece que hay un problema"
+  });
+}
+
+form.addEventListener("submit", handleSubmit);
 
     
 
